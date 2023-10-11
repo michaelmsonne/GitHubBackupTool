f752ea0b4c04da822a8d59091f45ad5fd68f98d5
Michael Morten Sonne <michael@sonnes.cloud>
2023-10-11 20:49:29 +0200

    Add migging code, cleanup and code comments

--- GithubBackup/Class/ApplicationEndStatus.cs
+++ GithubBackup/Class/ApplicationEndStatus.cs
@@ -27,23 +27,54 @@ namespace GithubBackup.Class
             // Calculate the elapsed time as a TimeSpan
             TimeSpan elapsedTime = endTime - startTime;
             Globals._elapsedTime = elapsedTime;
-                            
+            
+            // Display the TimeSpan value to the console
             Console.WriteLine("\nBackup Run Time: " + Globals._elapsedTime);
             Console.WriteLine("Backup start Time: " + Globals._startTime);
             Console.WriteLine("Backup end Time: " + Globals._endTime);
             Console.WriteLine("Errors: " + Globals._errors);
 
+            // Log the TimeSpan value to the log file
             Message("Backup Run Time: " + Globals._elapsedTime, EventType.Information, 1000);
             Message("Backup start Time: " + Globals._startTime, EventType.Information, 1000);
             Message("Backup end Time: " + Globals._endTime, EventType.Information, 1000);
             Message("Errors: " + Globals._errors, EventType.Information, 1000);
 
+            // Send email report if email options is set
             if (Globals._emailOptionsIsSet)
             {
-                ReportSender.SendEmail(Globals._mailserver, Globals._mailport, Globals._mailfrom, Globals._mailto, "DONE", Globals.repocountelements, Globals.repoitemscountelements, 0, 0, Globals._backupFolderName, Globals._elapsedTime, Globals._errors, Globals._totalBackupsIsDeleted, Globals._daysToKeepBackup, "TEST", "TEST", "TEST", Globals._useSimpleMailReportLayout, "TEST", Globals._startTime, Globals._endTime);
-                //Globals._mailserver, Globals._mailport, Globals._mailfrom, Globals._mailto, "emailStatus", "", 0, 0, 0, 0, 0, 0, Globals._backupFolderName, Globals._elapsedTime, Globals._errors, "", "", Globals._daysToKeepBackup, "", "", "", "", "", "", "", "", "", Globals._useSimpleMailReportLayout, "", "", Globals._startTime, Globals._endTime, 0, 0);
+                // Call method to send email report with status of backup
+                ReportSender.SendEmail(Globals._mailserver,
+                    Globals._mailport,
+                    Globals._mailfrom,
+                    Globals._mailto,
+                    "DONE",
+                    Globals.repocountelements,
+                    Globals.repoitemscountelements,
+                    0,
+                    0,
+                    Globals._backupFolderName,
+                    Globals._elapsedTime,
+                    Globals._errors,
+                    Globals._totalBackupsIsDeleted,
+                    Globals._daysToKeepBackup,
+                    "TEST",
+                    "TEST",
+                    "TEST",
+                    Globals._useSimpleMailReportLayout,
+                    "TEST",
+                    Globals._startTime,
+                    Globals._endTime);
+            }
+            else
+            {
+                Message("Not set to send email report - skipping", EventType.Information, 1000);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Not set to send email report - skipping");
+                Console.ResetColor();
             }
             
+            // Call method to end program
             Globals.ApplicationEndMessage();
         }
 
@@ -67,22 +98,54 @@ namespace GithubBackup.Class
             // Calculate the elapsed time as a TimeSpan
             TimeSpan elapsedTime = endTime - startTime;
             Globals._elapsedTime = elapsedTime;
-                            
+            
+            // Display the TimeSpan value to the console
             Console.WriteLine("\nBackup Run Time: " + Globals._elapsedTime);
             Console.WriteLine("Backup start Time: " + Globals._startTime);
             Console.WriteLine("Backup end Time: " + Globals._endTime);
             Console.WriteLine("Errors: " + Globals._errors);
 
+            // Log the TimeSpan value to the log file
             Message("Backup Run Time: " + Globals._elapsedTime, EventType.Information, 1000);
             Message("Backup start Time: " + Globals._startTime, EventType.Information, 1000);
             Message("Backup end Time: " + Globals._endTime, EventType.Information, 1000);
             Message("Errors: " + Globals._errors, EventType.Information, 1000);
 
+            // Send email report if email options is set
             if (Globals._emailOptionsIsSet)
             {
-                //ReportSender.SendEmail(Globals._mailserver, Globals._mailport, Globals._mailfrom, Globals._mailto, "emailStatus", "", 0, 0, 0, 0, 0, 0, Globals._backupFolderName, Globals._elapsedTime, Globals._errors, "", "", Globals._daysToKeepBackup, "", "", "", "", "", "", "", "", "", Globals._useSimpleMailReportLayout, "", "", Globals._startTime, Globals._endTime, 0, 0);
+                // Call method to send email report with status of backup
+                ReportSender.SendEmail(Globals._mailserver,
+                    Globals._mailport,
+                    Globals._mailfrom,
+                    Globals._mailto,
+                    "ERROR",
+                    Globals.repocountelements,
+                    Globals.repoitemscountelements,
+                    0,
+                    0,
+                    Globals._backupFolderName,
+                    Globals._elapsedTime,
+                    Globals._errors,
+                    Globals._totalBackupsIsDeleted,
+                    Globals._daysToKeepBackup,
+                    "TEST",
+                    "TEST",
+                    "TEST",
+                    Globals._useSimpleMailReportLayout,
+                    "TEST",
+                    Globals._startTime,
+                    Globals._endTime);
+            }
+            else
+            {
+                Message("Not set to send email report - skipping", EventType.Information, 1000);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Not set to send email report - skipping");
+                Console.ResetColor();
             }
             
+            // Call method to end program
             Globals.ApplicationEndMessage();
         }
     }
