3242793ef890e29089a6ff2740bd2eadf954b3ac
Michael Morten Sonne <49366751+michaelmsonne@users.noreply.github.com>
2024-03-09 11:49:24 +0100

    Typo in about tool in code and update changelog

--- GithubBackup/Commands/BackupCommand.cs
+++ GithubBackup/Commands/BackupCommand.cs
@@ -13,10 +13,10 @@ namespace GithubBackup.Commands
             // Define the main command and options for it and execute it if no sub-command is specified
             Command.UnrecognizedArgumentHandling = UnrecognizedArgumentHandling.Throw;
             Command.Name = "GitHubBackupTool";
-            Command.Description = "GitHub Backup for Git Projects and is using the API for GitHub.";
+            Command.Description = "GitHub Backup tool for Git Projects using the API to GitHub.";
             Command.ExtendedHelpText =  "\nMore information about this tool:" +
-                                        "\n  This tool creates a local backup of repositories of a given github user with some options to filter " +
-                                        "\n  based on some conditions." +
+                                        "\n  This tool creates a local backup of repositories of a given GitHub user with some options to filter " +
+                                        "\n  based on some conditions og backup type." +
                                         "\n" +
                                         "\n  While the code is perfectly safe on the GitHub infrastructure, there are cases where a centralized" +
                                         "\n  local backup of all projects and repositories is needed. These might include Corporate Policies," +
